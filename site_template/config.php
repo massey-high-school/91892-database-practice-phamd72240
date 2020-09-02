8d786a3a1e55e13471dbc6d5411599149f8d3e9b
phamd72240 <51727353+phamd72240@users.noreply.github.com>
2020-09-03 09:15:37 +1200

    LVL2_Database
    FOR VIDS

---
+++ site_template/config.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display _errors', 'on'); // change to 'off' when live 
+
+define ("DB_NAME" , "phamd72240");
+define ("DB_USERNAME","_phamd72240");
+define ("DB_PASSWORD","f317636f8282c5954231021c92d38ef8");
+define ("DB_HOST","localhost");
+
+?>
