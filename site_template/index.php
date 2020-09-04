3f4f45e8d2a39c7de36bee0bd40de0adebb63bec
phamd72240 <51727353+phamd72240@users.noreply.github.com>
2020-09-04 14:42:46 +1200

    4/09/20
    In case lockdown happens

--- site_template/index.php
+++ site_template/index.php
@@ -1,41 +1,5 @@
-<!DOCTYPE HTML>
-
-<html lang="en">
-    
-
-<head>
-    <meta charset="utf-8">
-    <meta name="description" content="games, apps, ">
-    <meta name="author" content="David Pham">
-    <meta name="keywords" content="games, apps, ratings">
-    
-    <title>Game Database</title>
-
-    <!-- for multiple fonts change | to %7c * no spaces*  -->
-    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet">  
-
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/data_style.css">    <!-- custom style sheet -->
-
-    
-</head>
-
-<body>
-    
-    <p class="message">Eek!  Your browser does not support grid.  Please upgrade your system.</p>
-        
-    <div class="wrapper">
-    
-        <!-- logo / small image goes here -->
-        <div class="box logo">
-            <a href="index.php"><img src="images/logo.png" width="261" height="150" alt="Dice" /></a>
-        </div> <!-- / logo -->
-        
-        <div class="box banner">
-            <h1>Games Database</h1>        
-        </div> <!-- / banner -->
+<?php include("topbit.php") ?>
                        
-            
         <div class="box main">
             <h2>Welcome</h2>
             
@@ -48,21 +12,4 @@
             
         </div> <!-- / main -->
         
-        <div class="box side">
-           
-           <h2>Search Area</h2>
-           
-           <p>This is where the search area goes
-
-            
-        </div> <!-- / side bar -->
-        
-        <div class="box footer">
-            CC David Pham 2020
-        </div> <!-- / footer -->
-                
-        
-    </div> <!-- / wrapper -->
-    
-            
-</body>
+<?php include("bottombit.php") ?>
