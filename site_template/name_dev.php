30c04d92928f62e7ca1142e4178f492db1a36c24
phamd72240 <51727353+phamd72240@users.noreply.github.com>
2020-09-25 14:20:02 +1200

    Create name_dev.php

---
+++ site_template/name_dev.php
@@ -0,0 +1,25 @@
+<?php include("topbit.php"); 
+
+  $name_dev = $_POST['dev_name'];
+
+ $find_sql = "SELECT * FROM `L2_prac_game_details` 
+ JOIN L2_prac_genre ON (L2_prac_game_details.GenreID = L2_prac_genre.GenreID)
+ JOIN L2_prac_developer ON (L2_prac_game_details.DeveloperID = L2_prac_developer.DeveloperID)
+ WHERE `Name` LIKE '%$name_dev%' OR `DevName` LIKE '%$name_dev%' 
+ ";
+ $find_query = mysqli_query($dbconnect, $find_sql);
+ $find_rs = mysqli_fetch_assoc($find_query);
+ $count = mysqli_num_rows($find_query);
+
+?>
+
+      <div class="box main">
+           <h2>Name / Developer Results</h2>
+          
+          <?php 
+          include ("results.php");
+          ?>
+        
+      </div> <!-- / main -->
+
+<?php include("bottombit.php") ?>
